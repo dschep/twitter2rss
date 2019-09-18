096cad2eab68d5706a39283557cf927720534de1
Daniel Schep <dschep@gmail.com>
2019-09-18 11:29:41 -0400

    sls enterprise/dashbord!

--- twitter2rss.js
+++ twitter2rss.js
@@ -3,7 +3,7 @@ const RSS = require('rss');
 const Promise = require('bluebird');
 
 
-var twitterToRss = module.exports.twitterToRss = (screenName) => {
+const twitterToRss = module.exports.twitterToRss = async screenName => {
   const client = Promise.promisifyAll(new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
@@ -17,29 +17,24 @@ var twitterToRss = module.exports.twitterToRss = (screenName) => {
     site_url: `https://twitter.com/${screenName}`,
   })
 
-  return client.getAsync('statuses/user_timeline', {screen_name: screenName})
-   .then((tweets, error) => {
-    if (error) {
-      feed.item({title: 'Error fetching statuses/user_timeline'})
-    } else {
-      tweets.forEach((tweet) => feed.item({
-        title: tweet.text,
-        description: tweet.text,
-        url: `https://twitter.com/${screenName}/status/${tweet.id_str}`,
-      }))
-    }
-    return feed;
-  });
+  const [tweets, error] = await client.getAsync('statuses/user_timeline', {screen_name: screenName});
+  if (error) {
+    feed.item({title: 'Error fetching statuses/user_timeline'})
+  } else {
+    tweets.forEach((tweet) => feed.item({
+      title: tweet.text,
+      description: tweet.text,
+      url: `https://twitter.com/${screenName}/status/${tweet.id_str}`,
+    }))
+  }
+  return feed;
 };
 
-module.exports.handler = (event, context, callback) => {
-  twitterToRss(event.pathParameters.screenName).then((feed) => {
-    const response = {
-      statusCode: 200,
-      body: feed.xml(),
-      headers: {'Content-Type': 'application/rss+xml'},
-    };
-
-  callback(null, response);
-  })
+module.exports.handler = async event => {
+  const feed = await twitterToRss(event.pathParameters.screenName);
+  return {
+    statusCode: 200,
+    body: feed.xml(),
+    headers: {'Content-Type': 'application/rss+xml'},
+  };
 };
